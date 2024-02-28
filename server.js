c885b9008b809d4bd5ae5bfe9caacef6eedbbcaf
DylanAbrahams <dylan@abrahams.nl>
2024-02-28 14:01:39 +0100

    Werkend post systeem

--- server.js
+++ server.js
@@ -2,19 +2,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const data = 
+[{
+  title:"Personenfilm",
+  story:"Heel leuk verhaal over een personage",
+  },
+];
+
+
 /* Middleware */
 app.set('view engine', 'ejs')
-app.use(express.static('static'))
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true})).use(express.static('static'))
 
 
 /* Routes */
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home', {data: data});
 })
 
-app.get('/register', (req, res) => {
-  res.send('Registreer Account')
-})
+
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  data.push ({
+    title:req.body.title,
+    story:req.body.story,
+  });
+  res.redirect('/')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
